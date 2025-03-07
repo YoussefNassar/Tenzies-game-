890d2d5cf079774ea512cc3baef3eff88971c030
Youssef Nassar <youssef.nassar@solactive.com>
2025-03-07 12:32:15 +0100

    Add a roll dice button

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import './App.css'
 import Die from "./components/Die.jsx";
+import {useState} from "react";
 
 function App() {
+    const [dice, setDice] = useState(generateAllNewDice())
+
+    function generateAllNewDice() {
+        return new Array(10)
+        .fill(0)
+        .map(() => Math.ceil(Math.random() * 6))
+    }
+
+    const diceElements = dice.map(num => <Die value={num} />)
+
     return (
         <main>
             <div className="dice-container">
-                {[...Array(10)].map((x, i) =>
-                    <Die value={(i % 6)+1}/>
-                )}
+                {diceElements}
             </div>
+            <button className="roll-dice" onClick={() => setDice(generateAllNewDice())}>Roll Dice</button>
         </main>
-
     )
 }
 
