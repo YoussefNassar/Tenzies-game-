80bd866ff52117a8b75448d1ca9bade180aa8ee3
Youssef Nassar <youssef.nassar@solactive.com>
2025-03-07 10:39:28 +0100

    add die component and add its styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
+import Die from "./components/Die.jsx";
 
 function App() {
+    return (
+        <main>
+            <div className="dice-container">
+                {[...Array(10)].map((x, i) =>
+                    <Die value={(i % 6)+1}/>
+                )}
+            </div>
+        </main>
 
-  return (
-    <main>
-    </main>
-  )
+    )
 }
 
 export default App
