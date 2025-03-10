5a6204233ccf45526f86f30c8014c06ce0067b3e
Youssef.Nassar <yossef41997@gmail.com>
2025-03-10 23:45:49 +0300

    add hold functionality to the dice

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,34 @@ function App() {
 
     function generateAllNewDice() {
         return new Array(10)
-        .fill(0)
-        .map(() => ({
-            value: Math.ceil(Math.random() * 6),
-            isHeld: false,
-            id: nanoid()
-        }))
+            .fill(0)
+            .map(() => ({
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false,
+                id: nanoid()
+            }))
     }
 
     function rollDice() {
         setDice(generateAllNewDice())
     }
 
-    const diceElements = dice.map(dieObj => <Die key={dieObj.id} value={dieObj.value} />)
+    function hold(id) {
+        setDice(oldDice => oldDice.map(die =>
+            die.id === id ?
+                { ...die, isHeld: !die.isHeld } :
+                die
+        ))
+    }
+
+    const diceElements = dice.map(dieObj => (
+        <Die
+            key={dieObj.id}
+            value={dieObj.value}
+            isHeld={dieObj.isHeld}
+            hold={() => hold(dieObj.id)}
+        />
+    ))
 
     return (
         <main>
